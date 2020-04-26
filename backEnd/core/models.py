9cd8817d4327b2b537f16d2eb8e9c60133cb98e5
Dayashkan <dayashkan.99@gmail.com>
2020-04-26 22:17:31 +0600

    project

---
+++ backEnd/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Articles(models.Model):
+    create_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Статью'
+        verbose_name_plural = 'Статьи'
+
+
+
